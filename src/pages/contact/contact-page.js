66123ae1d19dd15056f05e96ba9e0d66a088609b
Stanley Hayford <hayfordstanley@gmail.com>
2022-07-16 09:59:21 +0000

    Add contact

--- src/pages/contact/contact-page.js
+++ src/pages/contact/contact-page.js
@@ -1,9 +1,41 @@
 import Layout from "../../components/layout/layout";
-import {Box, Card, CardContent, Container, Grid, Link, Stack, Typography} from "@mui/material";
+import {
+    Box, Button,
+    Card,
+    CardContent,
+    Container,
+    FormControl, FormHelperText,
+    Grid,
+    InputAdornment,
+    InputLabel,
+    Link, OutlinedInput,
+    Stack,
+    Typography
+} from "@mui/material";
 import aicc from "../../assets/images/aicc.jpg";
-import {Call, Mail, Web} from "@mui/icons-material";
+import {Call, Mail, MailOutline, PersonOutline, Web} from "@mui/icons-material";
+import * as yup from "yup";
+import contactLogo from "./../../assets/images/contact-us.png";
+import {useFormik} from "formik";
 
 const ContactPage = () => {
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+        },
+        onSubmit: (values, {resetForm, setSubmitting}) => {
+
+        },
+        validateOnBlur: true,
+        validateOnChange: true,
+        validationSchema: yup.object({
+            email: yup.string().email('Invalid email').required('email required'),
+            name: yup.string().required('name required'),
+            message: yup.string().required('Message required'),
+        })
+    });
 
     return (
         <Layout>
@@ -182,8 +214,184 @@ const ContactPage = () => {
                 </Container>
             </Box>
 
-            <Box sx={{backgroundColor: 'background.default'}}>
+            <Box sx={{backgroundColor: 'background.default', py: 5}}>
                 <Container>
+                    <Typography variant="h4" sx={{color: 'text.primary', fontWeight: 'bold', mb: 2}}>
+                        Send Us an Email
+                    </Typography>
+                    <Grid container={true} spacing={4}>
+                        <Grid item={true} xs={12} md={6}>
+                            <Card variant="elevation" elevation={0}>
+                                <CardContent>
+                                    <Stack direction="column" spacing={2}>
+                                        <form autoComplete="off" onSubmit={formik.handleSubmit}>
+
+                                            <Box mb={4}>
+                                                <Typography
+                                                    mb={1} variant="body2"
+                                                    sx={{color: 'secondary.main', fontWeight: 'bold'}}>
+                                                    Name
+                                                </Typography>
+                                                <FormControl fullWidth={true} variant="outlined">
+                                                    <InputLabel htmlFor="name">Name</InputLabel>
+                                                    <OutlinedInput
+                                                        fullWidth={true}
+                                                        value={formik.values.name}
+                                                        id="name"
+                                                        name="name"
+                                                        type="text"
+                                                        endAdornment={
+                                                            <InputAdornment
+                                                                position="end">
+                                                                <PersonOutline
+                                                                    sx={{
+                                                                        cursor: 'pointer',
+                                                                        color: 'secondary.main',
+                                                                        padding: 1,
+                                                                        fontSize: 24,
+                                                                    }}
+                                                                />
+                                                            </InputAdornment>
+                                                        }
+                                                        error={formik.touched.name && formik.errors.name}
+                                                        onChange={formik.handleChange}
+                                                        onBlur={formik.handleBlur}
+                                                        placeholder="Enter name"
+                                                        required={true}
+                                                        label="Name"
+                                                        size="medium"
+                                                        margin="dense"
+                                                    />
+                                                    {formik.touched.name && formik.errors.name && (
+                                                        <FormHelperText
+                                                            error={true}>
+                                                            {formik.errors.name}
+                                                        </FormHelperText>
+                                                    )}
+                                                </FormControl>
+                                            </Box>
+
+                                            <Box mb={4}>
+                                                <Typography
+                                                    mb={1} variant="body2"
+                                                    sx={{color: 'secondary.main', fontWeight: 'bold'}}>
+                                                    Email
+                                                </Typography>
+                                                <FormControl fullWidth={true} variant="outlined">
+                                                    <InputLabel htmlFor="email">Email</InputLabel>
+                                                    <OutlinedInput
+                                                        fullWidth={true}
+                                                        value={formik.values.email}
+                                                        id="email"
+                                                        name="email"
+                                                        type="email"
+                                                        endAdornment={
+                                                            <InputAdornment
+                                                                position="end">
+                                                                <MailOutline
+                                                                    sx={{
+                                                                        cursor: 'pointer',
+                                                                        color: 'secondary.main',
+                                                                        padding: 1,
+                                                                        fontSize: 24,
+                                                                    }}
+                                                                />
+                                                            </InputAdornment>
+                                                        }
+                                                        error={formik.touched.email && formik.errors.email}
+                                                        onChange={formik.handleChange}
+                                                        onBlur={formik.handleBlur}
+                                                        placeholder="Enter username or email or phone"
+                                                        required={true}
+                                                        label="Email"
+                                                        size="medium"
+                                                        margin="dense"
+                                                    />
+                                                    {formik.touched.email && formik.errors.email && (
+                                                        <FormHelperText
+                                                            error={true}>
+                                                            {formik.errors.email}
+                                                        </FormHelperText>
+                                                    )}
+                                                </FormControl>
+                                            </Box>
+
+                                            <Box mb={4}>
+                                                <Typography
+                                                    mb={1} variant="body2"
+                                                    sx={{color: 'secondary.main', fontWeight: 'bold'}}>
+                                                    Message
+                                                </Typography>
+                                                <FormControl fullWidth={true} variant="outlined">
+                                                    <InputLabel htmlFor="message">Message</InputLabel>
+                                                    <OutlinedInput
+                                                        fullWidth={true}
+                                                        id="message"
+                                                        value={formik.values.message}
+                                                        name="message"
+                                                        type="text"
+                                                        error={formik.touched.message && formik.errors.message}
+                                                        onChange={formik.handleChange}
+                                                        onBlur={formik.handleBlur}
+                                                        placeholder="Enter message"
+                                                        required={true}
+                                                        label="Message"
+                                                        size="medium"
+                                                        margin="dense"
+                                                        multiline={true}
+                                                        minRows={5}
+                                                    />
+                                                    {formik.touched.password && formik.errors.message && (
+                                                        <FormHelperText
+                                                            error={true}>
+                                                            {formik.errors.message}
+                                                        </FormHelperText>
+                                                    )}
+                                                </FormControl>
+                                            </Box>
+
+                                            <Button
+                                                type="submit"
+                                                size="large"
+                                                color="secondary"
+                                                sx={{
+                                                    textTransform: 'capitalize',
+                                                    py: 1.2
+                                                }}
+                                                fullWidth={true}
+                                                loadingPosition="start"
+                                                variant="contained"
+                                                disableElevation={true}>
+                                                Send message
+                                            </Button>
+                                        </form>
+                                    </Stack>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                        <Grid item={true} xs={12} md={6}>
+                            <Box
+                                sx={{
+                                    flex: 1,
+                                    maxHeight: '100vh',
+                                    backgroundColor: 'background.default'
+                                }}>
+                                <Stack sx={{height: '100%'}} justifyContent="center" alignItems="center">
+                                    <img
+                                        style={{
+                                            maxHeight: '100vh',
+                                            width: '100%',
+                                            height: '100%',
+                                            objectFit: 'cover',
+                                            objectPosition: 'center'
+                                        }}
+                                        alt=""
+                                        src={contactLogo}
+                                    />
+                                </Stack>
+                            </Box>
+                        </Grid>
+                    </Grid>
                 </Container>
             </Box>
 
